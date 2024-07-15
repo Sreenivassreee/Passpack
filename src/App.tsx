9129d8194a129c55ac081e4dd35dcde75a19b900
Sree <sree@Srees-MacBook-Air.local>
2024-07-15 20:41:25 +0530

    integrated the firebase firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 
 import './App.css'
 import { Login } from './components/login'
+import { collection, doc, getDoc } from "firebase/firestore";
+
+import { db } from './firebase';
+import { useEffect } from 'react';
 
 function App() {
-  
-
+  useEffect(() => {
+    const docRef = doc(db, "users", "ksreenvascse@gmail.com");
+    getDoc(docRef)
+      .then((querySnapshot) => {
+        const newData = querySnapshot.data();
+        console.log("todos---", newData);
+      })
+  }, [])
   return (
     <div >
-      <Login/>
+      <Login />
     </div>
   )
 }
